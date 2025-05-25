955df0a4e70bbf0b50978f2db800737272c00f37
mariobp-NHH <mblazquezdepaz@gmail.com>
2025-05-25 16:46:06 +0200

    reset search input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+const UNPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 const App = () => {
   const [word, setWord] = useState("");
+
   const handleSearchSubmit = (e) => {
     e.preventDefault(); // Correct spelling
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNPLASH_KEY}`)
+    .then((res) => res.json())
+    .then((data)=>{
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    setWord('');
   };
-  console.log(word);
+
   return (
     <div>
       <Header title="Images Gallery 3" />
