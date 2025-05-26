bcd3c59036214323e2e8eb71c01dc446a8710f32
mariobp-NHH <mblazquezdepaz@gmail.com>
2025-05-26 16:56:53 +0200

    fixed code with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
 
 const UNPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault(); // Correct spelling
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data)=>{
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     setWord('');
   };
 
