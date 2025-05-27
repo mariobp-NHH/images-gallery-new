fb96824a3f6774e5196868298cf99e44a3a529a0
mariobp-NHH <mblazquezdepaz@gmail.com>
2025-05-27 16:23:19 +0200

    save images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ const UNPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault(); // Correct spelling
@@ -16,7 +19,8 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
+        console.log(images);
       })
       .catch((err) => {
         console.log(err);
