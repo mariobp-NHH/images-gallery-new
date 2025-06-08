9ce6c357dfd2d77e728baf3bf287490067c08fe9
mariobp-NHH <mblazquezdepaz@gmail.com>
2025-06-08 18:35:18 +0200

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
 
 const UNPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -32,6 +33,7 @@ const App = () => {
     <div>
       <Header title="Images Gallery 3" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <ImageCard />
     </div>
   );
 };
