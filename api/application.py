1c341fcd113ab0df1253e640e41e443f4e5103ea
mariobp-NHH <mblazquezdepaz@gmail.com>
2025-06-16 11:20:33 +0200

    first commit

---
+++ api/application.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, request
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
+
+if not UNSPLASH_KEY:
+  raise EnvironmentError("Please....")
+
+
+application = Flask(__name__)
+
+@application.route('/')
+def home():
+  return "<h1>Welcome Home</h1>"
+
+@application.route('/new-image')
+def new_image():
+  word = request.args.get("query")
+  headers= {
+    "Accept-Version": "v1",
+    "Authorization": "Client-ID " + UNSPLASH_KEY
+  }
+  params = {
+    "query": word
+  }
+  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+  print(response.text)
+  data = response.json()
+  return data 
+
+if __name__=='__main__':
+  application.run(debug=True, host="0.0.0.0", port=5050)  
---
+++ frontend/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import Welcome from './components/Welcome';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const UNPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
+const App = () => {
+  const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault(); // Correct spelling
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([{ ...data, title: word }, ...images]);
+        console.log(images);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord('');
+  };
+
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
+  return (
+    <div>
+      <Header title="Images Gallery 3" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        {images.length ? (
+          <Row xd={1} md={2} lg={3}>
+            {images.map((image, i) => (
+              <Col key={i} className="pb-3">
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Welcome />
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default App;
